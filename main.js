bbc7f5b8dc0969c43edc259b2afe12dc4ba5e211
Osman Asanov <anilf.business@gmail.com>
2023-02-15 14:35:08 +0300

    JSyuken_030

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 "use strict";
 
-let test = '12345';
-alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
-
-let test1 = String(12345);
-alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
-
-let test2 = String(12345);
-alert(Number(test[0]) * Number(test[1]) * Number(test[2]) * Number(test[3]) * Number(test[4]));
-
-let test3 = 12345;
-alert(test[4] + test[3] + test[2] + test[1] + test[0]);
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
