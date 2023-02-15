08cc92d79587f55d75bdee08d9bf369fadcd4ac2
Osman Asanov <anilf.business@gmail.com>
2023-02-15 14:32:38 +0300

    jsyuken_029

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 "use strict";
 
-let str = 'abcde';
-alert(str[str.length - 1]);
+let test = '12345';
+alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
 
-let str1 = 'abcde';
-alert(str1[str1.length - 2]);
+let test1 = String(12345);
+alert(Number(test[0]) + Number(test[1]) + Number(test[2]) + Number(test[3]) + Number(test[4]));
 
-let str2 = 'abcde';
-alert(str2[str2.length - 3]);
+let test2 = String(12345);
+alert(Number(test[0]) * Number(test[1]) * Number(test[2]) * Number(test[3]) * Number(test[4]));
+
+let test3 = 12345;
+alert(test[4] + test[3] + test[2] + test[1] + test[0]);
